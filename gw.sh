bbe4b5ced63454cb800d8675b1355bcd5fb07abb
fox0 <red-fox0@yandex.ru>
2018-12-28 13:07:10 +0700

    Update gw.sh

--- gw.sh
+++ gw.sh
@@ -7,31 +7,31 @@ address 192.168.0.1
 netmask 255.255.255.0
 EOF
 ifup enp0s8
-#systemctl restart networking
+/etc/init.d/networking restart
 
 apt-get update
 apt-get upgrade -y
-apt-get install htop net-tools policykit-1 tor netfilter-persistent isc-dhcp-server -y
+apt-get install htop net-tools policykit-1 tor isc-dhcp-server -y
+#netfilter-persistent
 
 cat >> /etc/tor/torrc << EOF
 DNSPort 192.168.0.1:53
 EOF
-systemctl restart tor
-systemctl status tor | grep active
+/etc/init.d/tor restart
 
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A INPUT -s 192.168.0.0/24 -j ACCEPT
-iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-mkdir /etc/iptables/
-iptables-save > /etc/iptables/rules.v4
+#iptables -P INPUT DROP
+#iptables -P FORWARD DROP
+#iptables -A INPUT -i lo -j ACCEPT
+#iptables -A INPUT -s 192.168.0.0/24 -j ACCEPT
+#iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+#/etc/init.d/netfilter-persistent save
+#/etc/init.d/netfilter-persistent restart
 
 dpkg-reconfigure isc-dhcp-server
 #/etc/dhcp/dhcpd.conf
 #subnet 192.168.0.0 netmask 255.255.255.0 {
 #  option domain-name-servers 192.168.0.1;
 #  option domain-name "example.net";
+#  option routers 192.168.0.1;
 #}
-systemctl restart isc-dhcp-server
-systemctl status isc-dhcp-server | grep active
+/etc/init.d/isc-dhcp-server restart
